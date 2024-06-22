2c906a90c12f7ab1e6f5ae8e724773e407b4717e
antsuarz <uo270543@uniovi.es>
2024-06-22 15:05:50 +0200

    Dibujo de Consola

---
+++ R2D2-Campustic/Program.cs
@@ -0,0 +1,13 @@
+﻿// Prometo que en consola el dibujo se ve bien :-)
+Console.WriteLine("____________________________________________________________");
+Console.WriteLine("                   _______      _____   ______      _____    ");
+Console.WriteLine("          ___     |_   __ \\    / ___ `.|_   _ `.   / ___ `.\t");
+Console.WriteLine("         / ()\\      | |__) |  |_/___) |  | | `. \\ |_/___) |");
+Console.WriteLine("       _|_____|_    |  __ /    .'____.'  | |  | |  .'____.'  ");
+Console.WriteLine("      | | === | |  _| |  \\ \\_ / /_____  _| |_.' / / /_____  ");
+Console.WriteLine("      |_|  O  |_| |____| |___||_______||______.'  |_______|   ");
+Console.WriteLine("       ||  O  ||                                  \t\t\t");
+Console.WriteLine("       ||__*__||                                   \t\t\t");
+Console.WriteLine("      |~ \\___/ ~|                                  \t\t\t");
+Console.WriteLine("      /=\\ /=\\ /=\\                                  \t\t\t");
+Console.WriteLine("______[_]_[_]_[_]___________________________________________");
